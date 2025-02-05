4df02537848ed24a0cb33a0854effa453feb5feb
wxxz8008 <wxxz8008@gmail.com>
2025-02-05 23:45:37 +0900

    [Silver2] boj_11053_가장긴증가하는부분수열.py

---
+++ boj_11053_가장긴증가하는부분수열.py
@@ -0,0 +1,12 @@
+'''
+증가하는 부분 수열
+'''
+n = int(input())
+li = list(map(int, input().split()))
+
+dp = [1 for _ in range(n)]
+for i in range(n):
+    for j in range(i):
+        if li[i] > li[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
