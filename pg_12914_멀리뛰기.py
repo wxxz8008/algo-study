451567fc7b832cc539041b0e85aa86ce9eb1c4e0
wxxz8008 <wxxz8008@gmail.com>
2025-03-02 23:42:36 +0900

    [Level2] pg_12914_멀리뛰기.py

---
+++ pg_12914_멀리뛰기.py
@@ -0,0 +1,18 @@
+'''
+맨 처음은 완전탐색 고려하고,
+아니다 싶으면 바로 다른 방식 고려
+'''
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp[n] % 1234567
